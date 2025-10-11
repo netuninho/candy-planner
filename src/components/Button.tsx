484d29fd4bd08b38b45edd0e94fe55d0543b8a62
Manu <manuelass.dev@gmail.com>
2025-10-11 17:24:21 -0300

    feat: melhorando componente button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,44 @@
-import React from 'react'
-import '../assets/styles/components/Button.scss'
+import React from "react";
+import "../assets/styles/components/Button.scss";
 
 type ButtonProps = {
   text: string;
-  link: string;
+  link?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  ariaLabel?: string;
+  variant?: "primary" | "secondary" | "icon";
 };
 
-const Button: React.FC<ButtonProps> = ({ text, link }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  link,
+  onClick,
+  type = "button",
+  ariaLabel,
+  variant = "primary",
+}) => {
+  if (link) {
+    return (
+      <a
+        href={link}
+        className={`button ${variant}`}
+        aria-label={ariaLabel || text}
+      >
+        {text}
+      </a>
+    );
+  }
+
   return (
-    <a className="button" href={link}>
+    <button
+      type={type}
+      onClick={onClick}
+      className={`button ${variant}`}
+      aria-label={ariaLabel || text}
+    >
       {text}
-    </a>
+    </button>
   );
 };
 
