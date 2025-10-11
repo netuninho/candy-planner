0a1583151c0bb4e517f0d30fdbfe75ec51b253ae
Manu <manuelass.dev@gmail.com>
2025-10-10 22:58:26 -0300

    feat: nova pagina habitos

---
+++ src/pages/Habitos.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const Habitos = () => {
+  return (
+    <>
+      <Header />
+      <h1>Habitos</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default Habitos
