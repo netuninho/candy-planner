4de586f5b4246a839f98900adb55569b85125de3
Manu <manuelass.dev@gmail.com>
2025-10-13 22:52:45 -0300

    codigo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
